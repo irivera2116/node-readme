dcc032c62922e250db731d4a16b9fd9afcf1bd5d
irivera2116 <irivera2116@GMAIL.COM>
2023-05-30 09:34:53 -0400

    added prompt and installed inquierer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Give a short description of the project.',
+        },
+        {
+            type: 'input',
+            name: 'Table of Contents',
+            message: 'List the items in your table of contents.',
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Add installation intstructions.',
+        },
+
+
+    ])
+    .then((data) => {
+        const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
